04d1646375224af80a2322918eb808c9f63d0b11
Dario Alvarez <dario17231@gmail.com>
2015-10-02 11:41:20 +0100

    Changed the script, and other minor changes on the html and the ts file.

--- setUp.ps1
+++ setUp.ps1
@@ -4,14 +4,14 @@ Function PressKeyToContinue () {
     $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
     cls
 }
-Write-Host "Installing the TypeScript Definition manager package"
+Write-Host "Installing the TypeScript Definition manager for DefinitelyTyped package"
 npm install -g tsd@^0.6.0
 PressKeyToContinue
 Write-Host "Getting 'Angular2', 'es-promise', 'rx' and 'rx-lite'"
 tsd install angular2 es-promise rx rx-lite
 PressKeyToContinue
 Write-Host "Installing TypeScript"
-npm install -g typescript
+npm install -g typescript@">=1.5.0 <=1.6.0"
 PressKeyToContinue
 Write-Host "Compiling the app.ts file"
 tsc -m commonjs -t es5 --emitDecoratorMetadata --experimentalDecorators app.ts
